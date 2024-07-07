671411976c23064751581eb9fe0773dc77d1b2e7
vpdevabacus <developer@abacusmail.in>
2024-07-07 21:58:28 +0530

    made login api

--- routers/company.router.js
+++ routers/company.router.js
@@ -3,5 +3,7 @@ const router = require('express').Router();
 const Company = require('../controllers/company.controller')
 
 router.post('/signup', Company.CompanySave)
+router.post('/signin', Company.Login)
+
 
 module.exports = router;
