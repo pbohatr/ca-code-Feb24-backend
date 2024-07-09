b8970ffa9bdf18f8e4b0f87cde0ec3af1f7f8b49
vpdevabacus <developer@abacusmail.in>
2024-07-10 01:53:42 +0530

    added getallparties api || logic to verify the token is for same user

--- controllers/party.controller.js
+++ controllers/party.controller.js
@@ -3,13 +3,23 @@ const PartyService = require('../services/party.service');
 
 const PartySave = async (req, res) => {
     try {
-        await PartyService.addParty(req.body, req.params.id);
+        await PartyService.addParty(req.body, req.params.id, req.user._id);
         res.status(200).send("OK");
     } catch (error) {
         res.status(500).json(error);
     }
 };
 
+const getParties = async (req, res) => {
+    try {
+        const allData = await PartyService.getAllParties(req.params.id, req.user._id);
+        res.status(200).json(allData);
+    } catch (error) {
+        res.status(500).json(error);
+    }
+}
+
 module.exports = {
     PartySave,
-      };
+    getParties
+};
--- services/party.service.js
+++ services/party.service.js
@@ -3,18 +3,35 @@ const Firm = require('../model/firm.schema');
 const PasswordService = require('./password.service');
 
 let service = {};
-service.addParty = addParty;;
+service.addParty = addParty;
+service.getAllParties = getAllParties;
 
-async function addParty(body, id) {
+async function addParty(body, id, companyId) {
     try {
         const existingFirm = await Firm.findById(id);
         if (!existingFirm) {
             return Promise.reject("Firm not found!");
         }
+        if (existingFirm.companyId.toString() !== companyId.toString()) {
+            return Promise.reject("Not authorized!");
+        }
         await Party.create({ ...body, firmId: existingFirm._id });
         return true;
     } catch (error) {
         return Promise.reject("Unable to create party. Try again later!")
+    }
+};
+
+async function getAllParties(id, companyId) {
+    try {
+        const getFirm = await Firm.findById(id);
+        if (getFirm.companyId.toString() !== companyId.toString()) {
+            return Promise.reject("Not authorized!");
+        }
+        const allParties = await Party.find({ firmId: id });
+        return allParties;
+    } catch (error) {
+        return Promise.reject("Unable to get all party data. Try again later!")
     }
 }
 
