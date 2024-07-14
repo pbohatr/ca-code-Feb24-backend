4de6531ee60953f5d203282c11a09e49c9c875bb
vpdevabacus <developer@abacusmail.in>
2024-07-14 14:16:29 +0530

    made getcategory apis

--- controllers/category.schema.js
+++ controllers/category.schema.js
@@ -1,17 +1,44 @@
 const express = require('express');
-const CategoryService = require('../services/party.service');
+const CategoryService = require('../services/category.service');
 
 const CategorySave = async (req, res) => {
     try {
         console.log("req>>>",req.body)
-        console.log("reqparams>>>",req.params)
-        await CategoryService.addCategory(req.body,req.params.id);
-        res.status(200).send("OK");
+        const {id} = req.params
+        console.log("reqparams>>>",{id} )
+        const categoryData = await CategoryService.addCategory(req.body,req.params.id);
+        console.log("categoryData",categoryData)
+        res.status(200).send(categoryData);
     } catch (error) {
         res.status(500).json(error);
     }
 };
 
+const getCategory = async (req, res) => {
+    try {
+        const {id} = req.params
+        console.log("reqparams>>>",{id})
+        const categoryData = await CategoryService.getCategory(req.params.id);
+        console.log("categoryData",categoryData)
+        res.status(200).send(categoryData);
+    } catch (error) {
+        res.status(500).json(error);
+    }
+}
+
+
+const getAllCategory = async (req, res) => {
+    try {
+        const categoryData = await CategoryService.getAllCategory();
+        console.log("categoryData",categoryData)
+        res.status(200).send(categoryData);
+    } catch (error) {
+        res.status(500).json(error);
+    }
+}
+
 module.exports = {
     CategorySave,
+    getCategory,
+    getAllCategory
       };
